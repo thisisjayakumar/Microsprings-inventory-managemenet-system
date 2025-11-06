82f31335fee85282ff198b972d12c26a920eaf85
thisisjayakumar <jayagma032@gmail.com>
2025-11-06 10:47:36 +0530

    staticfiles dirs

--- microsprings_inventory_system/settings.py
+++ microsprings_inventory_system/settings.py
@@ -183,9 +183,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # if you have additional static directories
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),  # if you have additional static directories
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
